f421a679263a3348a88039d5ee157d2796cc62e2
hieupcm03 <hieupcm03@gmail.com>
2026-09-03 14:49:01 +0700

    feat: Add upload Excel file feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { FileUploader } from "./components/FileUploader";
+import type { ScheduleRow } from "./types/scheduletype";
+
 function App() {
+  const [scheduleData, setScheduleData] = useState<ScheduleRow[]>([]);
+
+  const handleDataLoaded = (data: ScheduleRow[]) => {
+    setScheduleData(data);
+    console.log("Dữ liệu bóc tách thành công:", data);
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
       <h1>UIT Scheduler</h1>
+
+      <FileUploader onDataLoaded={handleDataLoaded} />
+
+      {scheduleData.length > 0 && (
+        <div
+          style={{
+            marginTop: "20px",
+            padding: "15px",
+            backgroundColor: "#e6ffe6",
+            borderRadius: "8px",
+          }}
+        >
+          <p>
+            <strong>Thành công!</strong> Hệ thống đã nạp {scheduleData.length}{" "}
+            lớp học.
+          </p>
+          <p style={{ fontSize: "14px", color: "#555" }}>
+            * Nhấn F12 mở tab Console để xem cấu trúc mảng JSON.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
